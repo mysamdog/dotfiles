03db4436c0cfe235aa928fdf957895580ff718a9
Sam Burns <samburns157@gmail.com>
2018-04-25 21:55:55 -0700

    Switched to emacs

---
+++ emacs/.emacs.d/elpa/airline-themes-20170623.958/airline-themes-pkg.el
@@ -0,0 +1,8 @@
+(define-package "airline-themes" "20170623.958" "vim-airline themes for emacs powerline"
+  '((powerline "2.3"))
+  :keywords
+  '("evil" "mode-line" "powerline" "airline" "themes")
+  :url "http://github.com/AnthonyDiGirolamo/airline-themes")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/.emacs.d/elpa/rotate-20160909.136/rotate-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rotate" "20160909.136" "Rotate the layout of emacs" 'nil :commit "091b5ac4fc310773253efb317e3dbe8e46959ba6" :url "https://github.com/daichirata/emacs-rotate" :keywords '("window" "layout"))
